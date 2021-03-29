1ad493c1d4f76f8f9cb71dc3c3a13887ec19dd81
bunilo <gaiamiglino@gmail.com>
2021-03-29 19:03:28 +0200

    css profile settings
    Probleem opgelost voor afbeeldingen

--- Eigenmaaltijden/Pages/ProfileSettings.cshtml.cs
+++ Eigenmaaltijden/Pages/ProfileSettings.cshtml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Dapper;
 using Eigenmaaltijden.wwwroot.includes;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Hosting;
+
 
 namespace Eigenmaaltijden.Pages
 {
@@ -20,8 +25,14 @@ namespace Eigenmaaltijden.Pages
         [BindProperty]
         public string ProfilePhotoPath { get; set; }
         
+        public IFormFile uploadedImage { get; set; }
+        private readonly IWebHostEnvironment _environment;
+        
         Database db = new wwwroot.includes.Database();
 
+        public ProfileSettings(IWebHostEnvironment env) {
+            _environment = env;
+        }
 
         public void GetData()
         {
@@ -37,21 +48,21 @@ namespace Eigenmaaltijden.Pages
         public void OnPostProfileSettings()
         {
             Name = Request.Form["username"];
-            PhoneNumber = Request.Form["phonenumber"];
             Description = Request.Form["about"];
 
             var connection = db.Connect();
 
-            connection.Execute("UPDATE verkoper_profiel SET Name = @NAME, PhoneNumber = @PHONE, Description = @DESCRIPTION WHERE UserID = 1", new
+            connection.Execute("UPDATE verkoper_profiel SET Name = @NAME, Description = @DESCRIPTION WHERE UserID = 1", new
             {
                 NAME = Name,
                 PHONE = PhoneNumber,
                 DESCRIPTION = Description
             });
         }
-        public void OnGet()
-        {
-            
+        public async Task OnPostAsync() {
+            var exportPath = Path.Combine(_environment.WebRootPath, "uploads", uploadedImage.FileName);
+            using(Stream fileStream = new FileStream(exportPath, FileMode.Create))
+                await uploadedImage.CopyToAsync(fileStream);
         }
     }
 }
