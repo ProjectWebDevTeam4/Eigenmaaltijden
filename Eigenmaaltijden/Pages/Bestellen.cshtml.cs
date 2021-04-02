0462d87b929410f02a5e450c0575fe1081bd7b9f
Dsteunebrink <danisteunebrink@live.nl>
2021-04-02 04:19:12 +0200

    connection API

--- Eigenmaaltijden/Pages/Bestellen.cshtml.cs
+++ Eigenmaaltijden/Pages/Bestellen.cshtml.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Eigenmaaltijden.wwwroot.includes;
 
 namespace Eigenmaaltijden.Pages
 {
     public class BestellenModel : PageModel
     {
+
         public void OnGet()
         {
+
         }
     }
 }
---
+++ Eigenmaaltijden/wwwroot/includes/MailManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eigenmaaltijden.wwwroot.includes
+{
+    public class MailManager
+    {
+        private static string CallMailChimpApi()
+        {
+            using(var http = new HttpClient())
+            {   
+                http.DefaultRequestHeaders.Authorization = 
+                new AuthenticationHeaderValue("Basic", mailchimpapikey-us1);
+                content = await http.GetStringAsync(@"https://us1.api.mailchimp.com/3.0/lists");
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
