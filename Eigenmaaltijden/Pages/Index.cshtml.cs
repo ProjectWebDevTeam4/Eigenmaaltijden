0e0f69919421a862c419ff4f1e277f432b810595
Mathijs Hoving <mhoving@hdms.nl>
2021-04-11 14:35:33 +0200

    Revert "Search"

--- Eigenmaaltijden/Pages/Index.cshtml.cs
+++ Eigenmaaltijden/Pages/Index.cshtml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -16,16 +15,9 @@ namespace EigenMaaltijd.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public Database db = Database.get();
+        Database db = new Database();
         public bool isLoggedIn { get; set; }
 
-        public class Maaltijd {
-            public string url {get; set;}
-            public string Name {get; set;}
-            public string Image {get; set;}
-            
-        }
-        public List<Maaltijd> maaltijd = new List<Maaltijd>();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -35,41 +27,11 @@ namespace EigenMaaltijd.Pages
         public void OnGet()
         {
             isLoggedIn = db.loginCheck(HttpContext.Session.GetString("sessionid"), HttpContext.Session.GetString("uid"));
-            GetMaaltijden();
         }
 
         public string getName()
         {
-            return db.GetLoggedInUser().Name;
-        }
-
-        public IActionResult OnGetLogout()
-        {
-            db.LogoutUser();
-            HttpContext.Session.Clear();
-
-            return RedirectToPage("Index");
-        }
-
-        public void GetMaaltijden()
-        {
-            using var connection = db.Connect();
-            
-            var maaltijden = connection.Query("SELECT m.MealID, m.Name, m.PhotoPath FROM maaltijden m INNER JOIN maaltijd_info i ON m.MealID=i.MealID WHERE i.Availability=2 ORDER BY RAND() LIMIT 5");
-            
-            if (maaltijden.AsList().Count > 0)
-            {
-                foreach (var maal in maaltijden)
-                {
-                    var list = new Maaltijd();
-
-                    list.url = $"/meal?meal={maal.MealID}";
-                    list.Name = maal.Name;
-                    list.Image = maal.PhotoPath;
-                    
-                    maaltijd.Add(list);
-                }
-            }
+            return db.user.Name;
         }
     }
 }
